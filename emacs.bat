2efcc09d5d9a655b36e1088f1b3eb4bc11e6c341
repelliuss <officialrepelliuss@gmail.com>
2020-05-19 05:45:41 +0300

    Managing the path input from shell
    Now it properly opens the files in emacs!

--- emacs.bat
+++ emacs.bat
@@ -1,3 +1,4 @@
 @echo OFF
-for /f "delims=" %%i in ('wsl wslpath -a .') do set launch_path=%%i
-start wt wsl emacs %launch_path%
+set launch_path=%1
+for /f "delims=" %%i in ('wsl wslpath -a %launch_path%') do set launch_path=%%i
+start wt wsl emacs "%launch_path%"
