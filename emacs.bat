0211beabcae84ca2e9e5e5143a2816e28ae634f5
repelliuss <officialrepelliuss@gmail.com>
2020-05-19 05:14:43 +0300

    Updated syntax

--- emacs.bat
+++ emacs.bat
@@ -1,3 +1,3 @@
-@ECHO OFF
-for /f "delims=" %%i in ('wsl wslpath -a .') do set CURRENT_PATH=%%i
-start wt wsl emacs %CURRENT_PATH%
+@echo OFF
+for /f "delims=" %%i in ('wsl wslpath -a .') do set launch_path=%%i
+start wt wsl emacs %launch_path%
