7b4aebdddda927deb9472fcae98ffc47abf03b7f
repelliuss <officialrepelliuss@gmail.com>
2020-07-10 00:07:25 +0300

    Asking admin privileges for reg management
    It is required to manipulate registers through command line.
    Last created register file won't be deleted to show reg contents.

--- delete_from_context_menu.bat
+++ delete_from_context_menu.bat
@@ -1,5 +1,29 @@
 @echo off
 
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 set /p app="What is the app name you want to delete?: "
 
 if not [%app%] == [] (
